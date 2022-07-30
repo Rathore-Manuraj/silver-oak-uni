17cfb77770b48a93106dfaf08d64d92fe584ca7e
Manuraj <105096252+Rathore-Manuraj@users.noreply.github.com>
2022-07-30 14:19:03 +0530

    Add files via upload

---
+++ javascript/manu.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var name = "raj";
+console.log(name);
+
+let firstname = "manraj";
+console.log((typeof firstname));
+
+let num = 34.4;
+console.log((typeof num));
+
+let mark = 34;
+console.log(mark+6);
+
+let a=5
+b=5;
